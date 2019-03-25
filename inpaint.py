16915af345ee576f2c23702a97dc1ba726693482
krshrimali <kushashwaravishrimali@gmail.com>
2019-03-25 20:55:55 +0530

    Minor Edits

--- inpaint.py
+++ inpaint.py
@@ -1,5 +1,4 @@
-
-'''
+"""
 Usage:
   python3 inpaint.py [<image>]
 
@@ -7,30 +6,35 @@ Keys:
   SPACE - inpaint
   r     - reset the inpainting mask
   ESC   - exit
-'''
-
-from __future__ import print_function
-
+"""
 import numpy as np
 import cv2 as cv
 import argparse
+# TODO: Copy Sketcher class to inpaint.py
+# TODO: Remove below line
+from common import Sketcher
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i","--image", help = 'required image path file')
+# TODO: Add default argument
+ap.add_argument("-i", "--image", help = 'required image path file')
 args = vars(ap.parse_args())
 
 
-from common import Sketcher
-
 def main():
-    img = cv.imread(args["image"])
+    # Read image in color mode
+    img = cv.imread(args["image"], cv.IMREAD_COLOR)
 
+    # If image is not read properly, return error
     if img is None:
-        print('Failed to load image file:', args["image"])
+        print('Failed to load image file: {}'.format(args["image"]))
+        return
 
-
+    # Create a copy of original image
     img_mark = img.copy()
+    # Create a black copy of original image
+    # Acts as a mask
     mark = np.zeros(img.shape[:2], np.uint8)
+    # Create sketch using OpenCV Utility Class: Sketcher
     sketch = Sketcher('img', [img_mark, mark], lambda : ((255, 255, 255), 255))
 
     while True:
@@ -38,8 +42,10 @@ def main():
         if ch == 27:
             break
         if ch == ord(' '):
-            res = cv.inpaint(img_mark, mark, 3, cv.INPAINT_TELEA)
-            cv.imshow('inpaint', res)
+            # Use Algorithm proposed by Alexendra Telea: Fast Marching Method (2004)
+            # Reference: https://pdfs.semanticscholar.org/622d/5f432e515da69f8f220fb92b17c8426d0427.pdf
+            res = cv.inpaint(src=img_mark, inpaintMask=mark, inpaintRadius=3, flags=cv.INPAINT_TELEA)
+            cv.imshow('Inpaint Output', res)
         if ch == ord('r'):
             img_mark[:] = img
             mark[:] = 0
